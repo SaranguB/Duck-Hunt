fa100425a28974a290af6224881d2147b32183d8
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-07-29 12:04:55 +0530

    hi

---
+++ Duck Hunt/Source/Enemy/Controller/RedDuckController.cpp
@@ -0,0 +1,45 @@
+#include "../../Header/Enemy/Controller/RedDuckController.h"
+#include "../../Header/Enemy/EnemyConfig.h"
+#include "../../Header/Enemy/EnemyModel.h"
+#include "../../Header/Global/ServiceLocator.h"
+
+namespace Enemy
+{
+	namespace Controller
+	{
+		
+		RedDuckController::RedDuckController(EnemyType type) : public EnemyController(type)
+		{
+		}
+		RedDuckController::~RedDuckController()
+		{
+		}
+		void RedDuckController::Initialize()
+		{
+		}
+		void RedDuckController::MoveLeft()
+		{
+		}
+		void RedDuckController::MoveRight()
+		{
+		}
+		void RedDuckController::MoveUP()
+		{
+		}
+		void RedDuckController::MoveDown()
+		{
+		}
+		void RedDuckController::MoveDiagonalLeftUP()
+		{
+		}
+		void RedDuckController::MoveDiagonalRightUP()
+		{
+		}
+		void RedDuckController::MoveDiagonalRightDown()
+		{
+		}
+		void RedDuckController::MoveDiagonalLeftDown()
+		{
+		}
+	}
+}
