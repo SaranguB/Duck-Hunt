1a5e30065a1a683ac7d980241fea53f2e3821b46
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-08-05 15:47:30 +0530

    UIlate

--- Duck Hunt/Header/Gameplay/GameplayView.h
+++ Duck Hunt/Header/Gameplay/GameplayView.h
@@ -10,26 +10,7 @@ namespace Gameplay
 	private:
 
 		UI::UIElement::ImageView* gameplayImage;
-		UI::UIElement::TextView* enemiesKilledText;
-		UI::UIElement::TextView* playerAmmo;
-		UI::UIElement::TextView* playerScore;
-		UI::UIElement::TextView* playerLive;
-
 		sf::RenderWindow* gameWindow;
-
-		const float textYPosition = 15.f;
-		const float textXPosition = 60.f;
-
-		const float enemiesKilledTextXPosition = 60.f;
-		const float playerAmmoTextXPosition = 1600.f;
-
-		const float playerScoreTextYPosition = 1000.f;
-		const float playerLiveTextXposition = 1600.;
-
-		const float fontSize = 40.f;
-
-		const sf::Color textColor = sf::Color::White;
-
 
 	public:
 		GameplayView();
@@ -39,19 +20,7 @@ namespace Gameplay
 		void Update();
 		void Render();
 
-		void UpdateEnemiesKilledText();
-		void UpdatePlayerAmmoText();
-		void UpdatePlayerScore();
-		void UpdatePlayerLive();
-
-
 		void InitializeBackgroundImage();
-		void InitializeEnemiesKilledText();
-		void InitializePlayerAmmo();
-		void InitializePlayerLive();
-		void InitializePlayerScore();
-
-
 
 	};
 }
