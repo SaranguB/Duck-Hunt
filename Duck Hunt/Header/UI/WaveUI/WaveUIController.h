a86cee9f3f7567b2ee71d66f67397cfae36575e7
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-08-05 15:57:15 +0530

    wave

--- Duck Hunt/Header/UI/WaveUI/WaveUIController.h
+++ Duck Hunt/Header/UI/WaveUI/WaveUIController.h
@@ -1,2 +1,25 @@
 #pragma once
+#include "../../Header/UI/Interface/IUIController.h"
 
+namespace UI
+{
+	namespace WaveUI
+	{
+		class WaveUIController : public Interface::IUIController
+		{
+		private:
+
+
+		public:
+			WaveUIController();
+			~WaveUIController();
+
+			void Initialize() override;
+			void Update() override;
+			void Render() override;
+
+			void Show() override;
+
+		};
+	}
+}
---
+++ Duck Hunt/Source/UI/WaveUI/WaveUIController.cpp
@@ -0,0 +1,30 @@
+#include "../../Header/UI/WaveUI/WaveUIController.h"
+
+namespace UI
+{
+	namespace WaveUI
+	{
+		WaveUIController::WaveUIController()
+		{
+
+		}
+
+		WaveUIController::~WaveUIController()
+		{
+
+		}
+
+		void WaveUIController::Initialize()
+		{
+		}
+		void WaveUIController::Update()
+		{
+		}
+		void WaveUIController::Render()
+		{
+		}
+		void WaveUIController::Show()
+		{
+		}
+	}
+}
